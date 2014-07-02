89b7c5d0f70cd2b4f7a3bc5ffeb6a4a0d638860c
LinguList <mattis.list@posteo.de>
2014-07-03 00:34:21 +0200

    new sampa in echtzeit

--- dighl/setup.js
+++ dighl/setup.js
@@ -1,6 +1,6 @@
 loadModule('dighl/vendor/jquery.js');
 loadModule('dighl/vendor/jquery-ui.js');
 loadModule('dighl/utils.js');
-loadModule('dighl/sampa2ipa.js');
+loadModule('dighl/sampa.js');
 loadModule('dighl/edit.js');
 
